3b840b94f0c9e18aa1419f6d27770c9452a0ebaa
Konstantin Renner <rennerkonsti@gmail.com>
2012-10-19 00:35:46 +0200

    modified:   WTFile.py 	deleted:    WTFileWrapper.py
    Persistent Hashcash
    Cleanup
    Check WTFile against an hash

--- filesystem/WTFile.py
+++ filesystem/WTFile.py
@@ -1,6 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: ascii -*-
+
+"""
+Wolpertinger Filehashing lib
+
+"""
+
+__author__ = 'Konstantin Renner (rennerkonsti@gmail.com)'
+__copyright__ = 'Copyright (c) 2012 Konstantin Renner'
+__license__ = 'GPLv2'
+__version__ = '1.2.3' #Versioning: http://www.python.org/dev/peps/pep-0386/
+
+
+
 import os
 import hashlib
+import logging
+import pickle
 
+"""
+	Contains all relevnat File data
+"""
 class WTFile(object):
 	
 	
@@ -10,16 +30,80 @@ class WTFile(object):
 		self.createHash()
 		
 	def getHash(self):
-		return self.md5.digest()
+		return self.hash
 		
 	def getMTime(self):
 		return self.lastChange
 	
 	def createHash(self):
-		self.md5 = hashlib.md5()
+		md5 = hashlib.md5()
 		with open(self.path, 'rb') as data:
-			for chunk in iter(lambda: data.read(128*self.md5.block_size), b''):
-				self.md5.update(chunk)
-		return self.md5.digest()
+			for chunk in iter(lambda: data.read(128*md5.block_size), b''):
+				md5.update(chunk)
+		self.hash = md5.digest()
+		return self.hash
 		
+	def matches(self, hash):
+		return self.hash == hash
+
+"""
+	Namespace Functions and Cache
+"""	
+
+def getFileHash(path):
+	global cache
+	try:
+		if(os.path.getmtime(path) == cache[path].getMTime()):
+			logging.debug('Cache hit for ' + path)
+			return cache[path].getHash()
+		else:
+			logging.debug('Cached Hash for ' + path + ' is obsolete')
+			del cache[path]
+			return getFileHash(path)
+	except KeyError:
+		logging.debug('Creating new Hash for ' + path)
+		cache[path] = WTFile(path)
+		return getFileHash(path)
+	except NameError:
+		load()
+		return getFileHash(path)
+
+def getDirHashes(path):
+	hashes = []
+	for root, dirs, files in os.walk(path):
+		for name in files:
+			logging.debug('Scanning file ' + os.path.join(root, name))
+			hashes.append(getFileHash(os.path.join(root, name)))
+			
+	return hashes
 	
+"""
+	Persistent Hashcash
+"""
+def save():
+	logging.info('Saving ' + str(len(cache)) + ' Hashes')
+	with open('hashcache.pickle', 'wb') as hashcache:
+		pickle.dump(cache, hashcache, pickle.HIGHEST_PROTOCOL)
+
+def load():
+	global cache
+	try:
+		with open('hashcache.pickle', 'rb') as hashcache:
+			cache = pickle.load(hashcache)
+	except IOError:
+		cache = dict()
+		
+	logging.info('Loaded ' + str(len(cache)) + ' Hashes')
+    	
+"""
+	Tests the hashing function against the /usr directory
+"""
+		
+def test():
+	logging.basicConfig(level=logging.DEBUG)
+	hashes = getDirHashes('/home/konsti/Videos')
+	save()
+	pass
+	
+if __name__=='__main__':
+    test()
