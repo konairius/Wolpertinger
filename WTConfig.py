f8e9773ab58576937b3400afdd707db72123a764
konairius <rennerkonsti@gmail.com>
2013-01-25 02:12:57 +0100

    Changed License to MIT

--- WTConfig.py
+++ WTConfig.py
@@ -3,11 +3,6 @@ Created on Jan 24, 2013
 
 '''
 
-__author__ = 'Konstantin Renner (rennerkonsti@gmail.com)'
-__copyright__ = 'Copyright (c) 2012 Konstantin Renner'
-__license__ = 'GPLv2'
-__version__ = '0.0.1'
-
 import logging
 logger = logging.getLogger(__name__)
 
